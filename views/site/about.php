e7862f6da27812ce6727ef59512af5f851ae71fb
Kenyer Dominguez <kenyer.dominguez@ducatablet.com>
2015-04-24 23:34:50 -0430

    first commit

---
+++ views/site/about.php
@@ -0,0 +1,16 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+$this->title = 'Nosotros';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Esta es la página de Nosotros
+    </p>
+
+    <code><!--<?= __FILE__ ?>--></code>
+</div>
